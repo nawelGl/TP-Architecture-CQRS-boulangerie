0ac5708efb1e00356f0619a8d638880ac2cd770e
nawelGl <ghazal.nawel@gmail.com>
2026-03-23 11:05:19 +0100

    create spring project

---
+++ api-read/src/main/java/fr/upec/episen/grid/api_read/ApiReadApplication.java
@@ -0,0 +1,13 @@
+package fr.upec.episen.grid.api_read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReadApplication.class, args);
+	}
+
+}
---
+++ api-read/src/test/java/fr/upec/episen/grid/api_read/ApiReadApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upec.episen.grid.api_read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
