333bc9c70562fd136439f2df832ffdbd0a8e298b
nawelGl <ghazal.nawel@gmail.com>
2026-03-23 11:02:12 +0100

    create spring project

---
+++ api-write/src/main/java/fr/upec/episen/grid/api_write/ApiWriteApplication.java
@@ -0,0 +1,13 @@
+package fr.upec.episen.grid.api_write;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWriteApplication.class, args);
+	}
+
+}
---
+++ api-write/src/test/java/fr/upec/episen/grid/api_write/ApiWriteApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upec.episen.grid.api_write;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
